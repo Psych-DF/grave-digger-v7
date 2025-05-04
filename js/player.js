bc097b890561e90acdf4ec3e98345450cf238459
Psych-DF <95335322+Psych-DF@users.noreply.github.com>
2025-05-04 13:32:56 +1000

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,15 @@
+// js/player.js
+export const player = {
+  x: 250, // middle of 150
+  y: 250,
+  diamond: 0,
+  gold: 0,
+  silver: 0,
+  ore: 0,
+  stone: 0,
+  clay: 0,
+  bone: 0,
+  dirt: 0,
+  rock: 0,
+};
+
